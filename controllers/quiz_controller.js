ed3b57478adcf93bb33accdc08795be22bd7cb65
manu <manu@tridente.lan.net>
2015-08-03 17:46:32 +0200

    Multiples preguntas en la BBDD: lectura

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,27 @@
 
-// GET /quizes/question
-exports.question = function(req, res) {
-    res.render('quizes/question', {pregunta: 'Capital de Italia'});
+var models = require('../models/models.js');
+
+// GET /quizes
+exports.index = function(req, res) {
+    models.Quiz.findAll().then(function(quizes) {
+	res.render('quizes/index.ejs', { quizes: quizes });
+    });
 };
 
-// GET /quizes/answer
+// GET /quizes/:quizId
+exports.show = function(req, res) {
+    models.Quiz.find(req.params.quizId).then(function(quiz) {
+	res.render('quizes/show', { quiz: quiz });
+    });
+};
+
+// GET /quizes/:quizId/answer
 exports.answer = function(req, res) {
-    if (req.query.respuesta === 'Roma') {
-	res.render('quizes/answer', {respuesta: 'Correcto'});
-    } else {
-	res.render('quizes/answer', {respuesta: '¡Incorrecto!'});
-    }
+    models.Quiz.find(req.params.quizId).then(function(quiz) {
+	if (req.query.respuesta == quiz.respuesta) {
+	    res.render('quizes/answer', { quiz: quiz, respuesta: 'Correcto' });
+	} else {
+	    res.render('quizes/answer', { quiz: quiz, respuesta: 'Incorrecto' });
+	}
+    });
 };
--- models/models.js
+++ models/models.js
@@ -11,13 +11,17 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
 exports.Quiz = Quiz;
 
-sequelize.sync().success(function() {
-    Quiz.count().success(function(count) { 
+sequelize.sync().then(function() {
+    Quiz.count().then(function(count) { 
 	if (count === 0) {
 	    Quiz.create({
 		pregunta: 'Capital de Italia',
 		respuesta: 'Roma'
-	    }).success(function() { console.log('Base de datos inicializada'); });
+	    });
+	    Quiz.create({
+		pregunta: 'Capital de Portugal',
+		respuesta: 'Lisboa'
+	    }).then(function() { console.log('Base de datos inicializada'); });
 	}
     });
 });
