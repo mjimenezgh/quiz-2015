5d0005363d33290f568f4f2ce9695340560ae219
manu <manu@tridente.lan.net>
2015-08-12 12:59:56 +0200

    Tema 7: autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,12 @@
+// access to restricted URLs
+exports.loginRequired = function(req, res, next) {
+    if (req.session.user) {
+	next();
+    } else {
+	res.redirect('/login');
+    }
+};
+
 
 // GET /login
 exports.new = function(req, res) {
