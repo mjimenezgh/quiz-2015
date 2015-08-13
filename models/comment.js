d84cf18323a422729fa4d7c4075e5ecae0634805
manu <manu@tridente.lan.net>
2015-08-13 10:28:58 +0200

    Tema 18: moderación de comentarios

--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,9 @@ module.exports = function(sequelize, DataTypes) {
     return sequelize.define('Comment',
 			    { texto: { type: DataTypes.STRING,
 				       validate: { notEmpty: {msg: "-> Falta comentario"}}
-				     }
+				     },
+			      publicado: { type: DataTypes.BOOLEAN,
+					   defaultValue: false
+					 }
 			    });
-}
+};
